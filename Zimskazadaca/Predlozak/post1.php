b20be96c1bfafa45ca9b539ea7c9e783d28abdfa
spoljodj <spoljo1122@gmail.com>
2020-01-03 18:05:17 +0100

    poboljšao i prvi zadatak

--- Zimskazadaca/Predlozak/post1.php
+++ Zimskazadaca/Predlozak/post1.php
@@ -1,52 +1,8 @@
-      <form action="" method="POST">
-      <label for="broj">Upišite <?php echo 'Prvi broj';?>. broj:</label>
-      <input type="number"  name="broj[0]" size="10">
-      <label for="broj">Upišite <?php echo 'Drugi broj';?>. broj:</label>
-      <input type="number"  name="broj[1]" size="10">
-      <label for="broj">Upišite <?php echo 'Treći broj';?>. broj:</label>
-      <input type="number"  name="broj[2]" size="10">
-       <label for="broj">Upišite <?php echo 'Četvrti broj';?>. broj:</label>
-      <input type="number"  name="broj[3]" size="10">
-       <label for="broj">Upišite <?php echo 'Peti broj';?>. broj:</label>
-      <input type="number"  name="broj[4]" size="10">
-       <label for="broj">Upišite <?php echo 'Šesti broj';?>. broj:</label>
-      <input type="number"  name="broj[5]" size="10">
-       <label for="broj">Upišite <?php echo 'Sedmi broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Osmi broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Deveti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Deseti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Jedanaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvanaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Trinaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Četrnaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Petnaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Šestnaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Sedmanaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Osamnaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Devetnaesti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvadeseti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvadesetprvi broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvadesetdrugi broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvadesettreći broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-       <label for="broj">Upišite <?php echo 'Dvadesetčetvrti broj';?>. broj:</label>
-      <input type="number"  name="broj[]" size="10">
-      <input type="submit" value="Postavi" >
-      </form>
-    
+<?php
+ echo '<form action="" method="POST">';
+for($i=0;$i<24;$i++){
+    echo' <label for="broj">Upišite '. ($i+1) . '. broj:</label>
+    <input type="number" name="'. $i .'" id="0">';
+    }
+    echo '<input type="submit" value="Postavi" >';
+?>
--- Zimskazadaca/zadatak1.php
+++ Zimskazadaca/zadatak1.php
@@ -12,58 +12,9 @@
     <div class="large-12 small-12 columns">
       <div class="callout" style="min-height: 400px">
       <div> 
-      <form action="" method="POST">
-      <label for="broj">Upišite prvi broj:</label>
-      <input type="number" name="0" id="number"> </br>
-      <label for="broj">Upišite drugi broj:</label>
-      <input type="number" name="1" id="number"> </br>
-      <label for="broj">Upišite treći broj:</label>
-      <input type="number" name="2" id="number"> </br>
-      <label for="broj">Upišite četvrti broj:</label>
-      <input type="number" name="3" id="number"></br>
-      <label for="broj">Upišite peti broj:</label>
-      <input type="number" name="4" id="number"></br>
-      <label for="broj">Upišite šesti broj:</label>
-      <input type="number" name="5" id="number"></br>
-      <label for="broj">Upišite sedmi broj:</label>
-      <input type="number" name="6" id="number"></br>
-      <label for="broj">Upišite osmi broj:</label>
-      <input type="number" name="7" id="number"></br>
-      <label for="broj">Upišite deveti broj:</label>
-      <input type="number" name="8" id="number"></br>
-      <label for="broj">Upišite deseti broj:</label>
-      <input type="number" name="9" id="number"></br>
-      <label for="broj">Upišite jedanaesti broj:</label>
-      <input type="number" name="10" id="number"></br>
-      <label for="broj">Upišite dvanaesti broj:</label>
-      <input type="number" name="11" id="number"></br>
-      <label for="broj">Upišite trinaesti broj:</label>
-      <input type="number" name="12" id="number"></br>
-      <label for="broj">Upišite četrnaesti broj:</label>
-      <input type="number" name="13" id="number"></br>
-      <label for="broj">Upišite petnaesti broj:</label>
-      <input type="number" name="14" id="number"></br>
-      <label for="broj">Upišite šesnaesti broj:</label>
-      <input type="number" name="15" id="number"></br>
-      <label for="broj">Upišite sedamanaesti broj:</label>
-      <input type="number" name="16" id="number"></br>
-      <label for="broj">Upišite osamnaesti broj:</label>
-      <input type="number" name="17" id="number"></br>
-      <label for="broj">Upišite devetnaesti broj:</label>
-      <input type="number" name="18" id="number"></br>
-      <label for="broj">Upišite dvadeseti broj:</label>
-      <input type="number" name="19" id="number"></br>
-      <label for="broj">Upišite dvadesetprvi broj:</label>
-      <input type="number" name="20" id="number"></br>
-      <label for="broj">Upišite dvadestdrugi broj:</label>
-      <input type="number" name="21" id="number"></br>
-      <label for="broj">Upišite dvadesettreći broj:</label>
-      <input type="number" name="22" id="number"></br>
-      <label for="broj">Upišite dvadesetčetvrti broj:</label>
-      <input type="number" name="23" id="number"></br>
-      <input type="submit" value="Postavi" >
-      </form>
-    
+      <?php
+      include_once 'predlozak/post1.php';
+      ?>
       </div>
         <?php
         $zbroj=0;
